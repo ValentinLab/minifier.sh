ec4d33e6206797c7e0f655354eac3701bc31eab9
Valentin Perignon <valentin.perignon@orange.fr>
2020-02-15 21:17:38 +0100

    CSS minifier

--- minimifier.sh
+++ minimifier.sh
@@ -1,1 +1,11 @@
 #!/bin/dash
+
+# Exercice 3
+# Minifier un fichier CSS
+# Le chemin vers le fichier CSS correspond au premier argument
+
+# Destination folder
+DEST_FOLDER=result
+
+# CSS minifier
+tr -s '\n' ' ' < $1 | sed -r -e 's/[ ]*\/\*[^\/]+\/[ ]*//g' -e 's/\r|\t|\v//g' -e 's/[ ]*(:|;|,|\{|\}|>)[ ]*/\1/g' > $DEST_FOLDER/$1
